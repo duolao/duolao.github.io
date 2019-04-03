86079273f5c8d07112ba8aac75f583304ec6c346
w1539066264 <1539066264@qq.com>
2019-04-03 08:43:44 +0800

    asdf

---
+++ js/vip.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $("#wzr_look1").click(function(){
+        $("#wzr_tc_1").show();
+    });
+    $("#wzr_close").click(function(){
+        $("#wzr_tc_1").hide();
+    });
+ });
+
+ $(document).ready(function(){
+    $("#wzr_look2").click(function(){
+        $("#wzr_tc_2").show();
+    });
+    $("#wzr_colse2").click(function(){
+        $("#wzr_tc_2").hide();
+    });
+ });
